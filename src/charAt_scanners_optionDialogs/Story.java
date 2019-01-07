a6e31eba8f3cf66c1f7a664e7ff9d92971e058f2
OtisL <league@WTS13.attlocal.net>
2019-01-07 15:53:25 -0800

    DONE

--- src/charAt_scanners_optionDialogs/Story.java
+++ src/charAt_scanners_optionDialogs/Story.java
@@ -10,10 +10,15 @@ public class Story {
 			int b = JOptionPane.showOptionDialog(null, "You walk a bit further until you come to a divide. Where do you go?", "Adventure Story", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "Left", "Right"}, null);
 			if (b==0) {
-				int task = JOptionPane.showOptionDialog(null, "You are in a dark forest. You see a cave and nothing else. What do you do?", "Adventure Story", 0, JOptionPane.INFORMATION_MESSAGE, null,
-						new String[] { "Continue walking", "Go to cave"}, null);
+				int c = JOptionPane.showOptionDialog(null, "You walk for a long way and you start to get tired. Do you want to stop and rest?", "Adventure Story", 0, JOptionPane.INFORMATION_MESSAGE, null,
+						new String[] { "Yes", "No"}, null);
+				if (c==1) {
+					JOptionPane.showMessageDialog(null, "You keep walking even though you are tired. Finally, you reach the end and there is a small town there! You have been saved!", "Yay", JOptionPane.INFORMATION_MESSAGE);
+				}else {
+					JOptionPane.showMessageDialog(null, "You take a rest. But a creature in the cave devours you!", "Oops!", JOptionPane.ERROR_MESSAGE);
+				}
 			}else {
-				JOptionPane.showMessageDialog(null, "You continue to walk on the left part. Then you trip on the edge of a pit! You fall to your death.", "Oops!", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "You continue to walk on the right part. Then you trip on the edge of a pit! You fall to your death.", "Oops!", JOptionPane.ERROR_MESSAGE);
 			}
 		}else {
 			JOptionPane.showMessageDialog(null, "You approach a cliff and don't notice it until it's too late! You fall to your death", "Oops!", JOptionPane.ERROR_MESSAGE);
